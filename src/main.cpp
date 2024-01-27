c128e560ca97fecf7f5e37fc9eef31a48e3ea787
Merlin Nuñez <merlinsn@gmail.com>
2024-01-27 17:11:25 +0100

    first working commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,42 @@ const char* password = "sustancias123";
 WiFiUDP Udp;
 unsigned int localUdpPort = 4210;  // local port to listen on
 char incomingPacket[255];  // buffer for incoming packets
-char  replyPacket[] = "Hi there! Got the message :-)";  // a reply string to send back
+
+// Set your Static IP address
+IPAddress local_IP(192, 168, 0, 13);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 0, 1);
+
+IPAddress subnet(255, 255, 255, 0);
+
+// Motor Speed
+int ENA = D2;
+int IN1 = D7;
+int IN2 = D8;
+
+// Motor Dir
+int ENB = D1;
+int IN3 = D5;
+int IN4 = D6;
 
 
 void setup()
 {
+
+ pinMode (ENA, OUTPUT);
+ pinMode (ENB, OUTPUT);
+ pinMode (IN1, OUTPUT);
+ pinMode (IN2, OUTPUT);
+ pinMode (IN3, OUTPUT);
+ pinMode (IN4, OUTPUT);
+
   Serial.begin(115200);
   Serial.println();
+
+  // Configures static IP address
+if (!WiFi.config(local_IP, gateway, subnet)) {
+  Serial.println("STA Failed to configure");
+}
 
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
@@ -34,18 +63,40 @@ void loop()
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
-    // receive incoming UDP packets
-    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
     int len = Udp.read(incomingPacket, 255);
     if (len > 0)
     {
       incomingPacket[len] = 0;
     }
-    Serial.printf("UDP packet contents: %s\n", incomingPacket);
+    int speed = incomingPacket[1] - 48;
+    int direction = incomingPacket[0] - 48;
+    int reverse = incomingPacket[2] - 48;
 
-    // send back a reply, to the IP address and port we got the packet from
-    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    Udp.write(replyPacket);
-    Udp.endPacket();
+   if(speed == 0){
+     analogWrite(ENA, 0);
+   } else {
+     analogWrite(ENA, 50 + (speed * 25));
+   }
+    if(reverse == 1){
+       digitalWrite (IN1, HIGH);
+       digitalWrite (IN2, LOW);
+    } else if(reverse == 0){
+      digitalWrite (IN1, LOW);
+      digitalWrite (IN2, HIGH);
+    }
+
+    if(direction == 2){
+      analogWrite(ENB, 230);
+       digitalWrite (IN3, HIGH);
+       digitalWrite (IN4, LOW);
+    } else if(direction == 1){
+      analogWrite(ENB, 0);
+       digitalWrite (IN3, LOW);
+       digitalWrite (IN4, LOW);
+    } else if(direction == 0){
+      analogWrite(ENB, 230);
+       digitalWrite (IN3, LOW);
+       digitalWrite (IN4, HIGH);
+    }
   }
 }
